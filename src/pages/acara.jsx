8bbc5849fa61905c3a3ebc1b1a13d3d012b09071
FajarAntiHero <fajar030506.gmail.com>
2025-06-06 18:47:56 +0700

    adding pages, fix navbar and footer

---
+++ src/pages/acara.jsx
@@ -0,0 +1,11 @@
+import FooterContainer from "../component/footer";
+import NavbarContainer from "../component/navbar";
+
+export default function Acara(){
+    return (
+        <>
+            <NavbarContainer/>
+            <FooterContainer/>
+        </>
+    )
+}
